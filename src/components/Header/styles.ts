30595778dafc0f4db26487f22b874e58466408d4
hv90 <hugo.machado@hi.com.br>
2021-05-04 15:12:58 -0300

    add elementary sources

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,7 +3,15 @@ import styled from "styled-components";
 export const Container = styled.div`
   h1 {
     font-family: "PokeTitle";
+    font-size: 8vh;
+    color: #ffde00;
+    -webkit-text-stroke-width: 1.3px;
+    -webkit-text-stroke-color: #3b4cca;
+    text-shadow: -4px 3px 1px #273286;
+    margin: 0;
+    padding: 10px;
   }
+
   top: 0;
   left: 0;
   position: absolute;

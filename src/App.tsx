0a694f4ffd156529654b30894e1ad674b26fb153
hv90 <hugo.machado@hi.com.br>
2021-05-05 17:16:20 -0300

    add loading and NoResultCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card, { CardProps } from 'components/Card';
 import Header from 'components/Header'; /*
 import CardDefaultProps from 'mock/card'; */
 import SearchBar from 'components/SearchBar';
+import spinner from 'static/images/loader.png';
+import { LoaderContainer, NoResultCard } from './Styles';
 
 const App: React.FC = () => {
   const [data, setData] = useState<CardProps>();
   const [typedSearch, setTypedSearch] = useState('');
+  const [loading, setLoading] = useState(false);
   const handleData = (pokemonData: CardProps, word: string) => {
     setData(pokemonData);
     setTypedSearch(word);
   };
+  useEffect(() => {
+    // do nothing
+  }, [typedSearch]);
   return (
     <div>
       <Header />
@@ -23,13 +29,25 @@ const App: React.FC = () => {
         }}
       >
         <SearchBar
-          onChange={(pokemonData: CardProps, word: string) =>
-            handleData(pokemonData, word)
-          }
+          onChange={(pokemonData: CardProps, word: string) => {
+            return handleData(pokemonData, word);
+          }}
+          isFetching={(isFetching: boolean) => setLoading(isFetching)}
         />
 
-        {data && <Card {...data} />}
-        {typedSearch && !data && <p className="noResult">No PokéDex matches</p>}
+        {typedSearch !== '' && !loading && data && !(data?.number === -666) && (
+          <Card {...data} />
+        )}
+        {typedSearch !== '' && loading && (
+          <LoaderContainer>
+            <img className="spinner" src={spinner} alt="loading..." />
+          </LoaderContainer>
+        )}
+        {typedSearch !== '' && !loading && data?.number === -666 && (
+          <NoResultCard>
+            <p className="noResult">No PokéDex matches</p>
+          </NoResultCard>
+        )}
       </div>
     </div>
   );
--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -8,41 +8,56 @@ import { Container } from './styles';
 
 interface Props {
   onChange: (d: CardProps, w: string) => void;
+  isFetching: (i: boolean) => void;
 }
 
-const SearchBar: React.FC<Props> = ({ onChange }: Props) => {
+const SearchBar: React.FC<Props> = ({ onChange, isFetching }: Props) => {
   const [typedSearch, setTypedSearch] = useState('');
   const [data, setData] = useState<CardProps>();
-
   let debounceTime: any;
 
   const debounceTyping = (typedWord: string) => {
     clearTimeout(debounceTime);
-
+    isFetching(true);
     debounceTime = setTimeout(() => {
       setTypedSearch(typedWord);
-      console.log(typedSearch);
+      isFetching(false);
     }, 1000);
   };
 
   useEffect(() => {
-    if (typedSearch !== '') {
-      api.get<IPokemon>(`/pokemon/${typedSearch}`).then(res => {
-        if (res.data.name !== '') {
-          api
-            .get<IPokemonSpecies>(`/pokemon-species/${res.data.name}`)
-            .then(result => {
-              setData({
-                number: res.data.id,
-                name: res.data.name,
-                types: res.data.types.map(type => type.type.name),
-                sprite: res.data.sprites.front_default,
-                flavorData: result.data.flavor_text_entries,
-              });
-            });
+    const getData = async () => {
+      if (typedSearch !== '') {
+        setData({
+          number: -666,
+          name: '',
+          types: [],
+          sprite: '',
+          flavorData: [],
+        });
+        try {
+          await api.get<IPokemon>(`/pokemon/${typedSearch}`).then(async res => {
+            if (res.data.name !== '') {
+              await api
+                .get<IPokemonSpecies>(`/pokemon-species/${res.data.name}`)
+                .then(result => {
+                  setData({
+                    number: res.data.id,
+                    name: res.data.name,
+                    types: res.data.types.map(type => type.type.name),
+                    sprite: res.data.sprites.front_default,
+                    flavorData: result.data.flavor_text_entries,
+                  });
+                });
+            }
+          });
+          isFetching(false);
+        } catch (e) {
+          console.error(e);
         }
-      });
-    }
+      }
+    };
+    getData();
   }, [typedSearch]);
   return (
     <Container>
@@ -55,7 +70,7 @@ const SearchBar: React.FC<Props> = ({ onChange }: Props) => {
         />
         <AiOutlineSearch className="searchIcon" />
       </div>
-      {data && onChange(data, typedSearch)}
+      {data && onChange(data, typedSearch) && isFetching(false)}
     </Container>
   );
 };

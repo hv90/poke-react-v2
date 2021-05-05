4b3d5fc47a73fe7960dfafd49cdaf550f7e41564
Victor Hofstetter <victor.hofstetter@hi.com.br>
2021-05-04 22:50:08 -0300

    eslint + prettier + airbnb config

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,12 @@
-import React from "react";
+import React from 'react';
 
-import logo from "../../assets/images/logo.png";
-import { Container } from "./styles";
+import logo from '../../assets/images/logo.png';
+import { Container } from './styles';
 
-const Header: React.FC = () => {
-  return (
-    <Container>
-      <img src={logo} alt="header logo" />
-    </Container>
-  );
-};
+const Header: React.FC = () => (
+  <Container>
+    <img src={logo} alt="header logo" />
+  </Container>
+);
 
 export default Header;
--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,12 +1,12 @@
-import { css } from "styled-components";
+import { css } from 'styled-components';
 
 const fonts = css`
   @font-face {
-    font-family: "FireRed";
+    font-family: 'FireRed';
     font-style: normal;
     font-weight: normal;
-    src: local("Pokemon Fire Red Regular"),
-      url("pokemon_fire_red.woff") format("woff");
+    src: local('Pokemon Fire Red Regular'),
+      url('pokemon_fire_red.woff') format('woff');
   }
 `;
 
